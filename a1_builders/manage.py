91cfc801f643cdf5678f7b5ee7b4afa0e0196fa3
cbm210 <camilobentrenandolatinos@gmail.com>
2024-03-27 11:13:55 -0500

    Release Environment

--- a1_builders/manage.py
+++ a1_builders/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a1_builders.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a1_builders.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
